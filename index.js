cc28301c1b6ac3dba89f50af73ac206fe25afdd5
siddheshkadam21 <sandy9029@outlook.com>
2023-03-26 01:07:16 +0530

    srtings and functions

--- index.js
+++ index.js
@@ -149,7 +149,8 @@ do{
     console.log("Do while loop output",i)
     
 }while(i<n)
-*/
+
+
 //function
 //tradtional way to writ function
 const prompt =require("prompt-sync")();// importing
@@ -174,3 +175,53 @@ console.log("average of number",average_num(first_number,second_number));
 console.log(summation_of_numbers(first_number,second_number));
 
 //video12
+let marks = {
+    a:10,
+    b:20,
+    c:30,
+    d:40
+};
+for (const pro in marks){
+    console.log(`${pro}:${marks[pro]}`);
+}
+//console.log(marks)
+const mean = (a,b,c) =>{
+    return (a+b+c)/3
+};
+console.log(mean(2,2,2));
+*/
+//string- inserting in sentence using back strick
+//srting interpulation
+//string is immutable
+let fn = "First Name"
+let ln = "Last Name"
+console.log(`Enter your ${fn} and ${ln}`)
+let fri = 'water\'melon'
+let doublecode = "water'Melon'"
+console.log(fri)
+console.log(doublecode)
+console.log("Number of characters:",fn.length)//number of characters
+console.log("Upper case:",fn.toUpperCase())//upper case
+console.log("Lower case:",fn.toLowerCase())//lower case
+console.log("Indexing with two argument:",fn.slice(2,5))//indexing
+console.log("Indexing with one argument:",fn.slice(2))
+console.log("Replace:",fn.replace("First","Your"))//replace character
+console.log("After entering ",fn.concat(" also enter your ",ln.toUpperCase()," and then DOB"))//concat
+let rt = "    SK      "
+console.log("Removed space which is present at start and end",rt.trim()) // remove extra space from start and end middle will remain same
+
+//video 15
+console.log(`Enter "${ln}"${fn.includes(ln)? ' middle name ': 'DOB'} Submit`); //includes return true or false
+
+//extracting specific word or number from sentence using indexcing for it.
+let srt = "The given amount is 25000";
+let amount = Number.parseInt(srt.slice("The given amount is".length));
+console.log(amount);
+console.log(typeof amount)
+
+//as string is immutable it won't change
+let friend = "Elon"
+friend[2] = "M"
+console.log(friend)
+
+//video16 - Array
